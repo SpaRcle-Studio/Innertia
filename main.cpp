87a345e892e1dce3c94fbd4e008b34fedca9d3fc
Monika0000 <rotaru5craft@gmail.com>
2024-02-19 22:25:05 +0100

    (Socket) Added socket from utils.

--- main.cpp
+++ main.cpp
@@ -1,18 +1,28 @@
 #include <Utils/Debug.h>
 #include <Utils/Platform/Platform.h>
+#include <Utils/Network/Socket.h>
+#include <Utils/Locale/Encoding.h>
 
 #include <Core/Block.h>
-
-#include <asio/ip/tcp.hpp>
-#include <asio/io_context.hpp>
 
 int main(int argc, char*argv[]) {
     auto&& applicationPath = SR_PLATFORM_NS::GetApplicationPath().GetFolder();
     SR_UTILS_NS::Debug::Instance().Init(applicationPath, true, SR_UTILS_NS::Debug::Theme::Dark);
 
-    SR_LOG("Hello, World!");
+    SR_UTILS_NS::Locale::SetLocale();
 
-    IR_TYPES_NS::uint256_t hash = "2938472983479283742038746283746283746287346729363475638475682743";
+    SR_LOG("Starting application...");
+
+    auto&& pSocket = SR_NETWORK_NS::Socket::Create(SR_NETWORK_NS::SocketType::TCP);
+    if (!pSocket->Connect("127.0.0.1", 80)) {
+        SR_LOG("Failed to connect to address");
+        return -1;
+    }
+    else {
+        SR_LOG("Connected!");
+    }
+
+    /*IR_TYPES_NS::uint256_t hash = "2938472983479283742038746283746283746287346729363475638475682743";
 
     std::cout << hash << std::endl;
 
@@ -31,7 +41,7 @@ int main(int argc, char*argv[]) {
     }
     else {
         std::cout << "Failed to connect to address: " << errorCode.message() << std::endl;
-    }
+    }*/
 
     return 0;
 }
