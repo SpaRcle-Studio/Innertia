10930278d237d06c94cb40277b8850d434221a3d
qulop <qlopsimail@gmail.com>
2024-02-21 03:38:40 +0300

    (Core) Some edits in Block.

--- inc/Core/Block.h
+++ inc/Core/Block.h
@@ -13,35 +13,43 @@
 #include <Utils/Types/Hash.h>
 
 
-#define HASH_SIZE   256
-
 
 namespace IR_CORE_NS {
     class Block {
         // TODO: implement sha256 and replace this V
         using Hash = IR_TYPES_NS::Hash<SR_TYPES_NS::uint256_t>;
     public:
-        Block(std::string&& minedTime, uint64_t diffuculty, Hash* hash, Hash* m_previousBlock, uint64_t nonce);
+        Block(const std::string& timestamp, uint64_t diffuculty, Hash* hash, Hash* previousBlock, uint64_t nonce);
 
         IR_NODISCARD Hash* GetHash() const;
         
+        IR_NODISCARD Hash* GetPreviousBlockHash() const;
+        
         IR_NODISCARD uint64_t GetNonce() const;
+        
+        IR_NODISCARD std::string GetTimestamp() const;
+        
+        IR_NODISCARD uint64_t GetDifficulty() const;
+        
+        IR_NODISCARD uint64_t GetSize() const;
 
         IR_NODISCARD std::string ToString() const;
 
         ~Block();
 
+
     private:
-        std::string m_minedTime;
-        uint64_t m_difficulty = 0;
-
-        Hash* m_hash = nullptr;
+        // Header
         Hash* m_previousBlock = nullptr;
-        // SR_MATH_NS::TreeT<256> m_merkleHash;      // TODO: sha256
-        uint64_t m_size = 0;
+        std::string m_timestamp;
+        uint64_t m_difficulty = 0;
         uint64_t m_nonce = 0;
+        // SR_MATH_NS::TreeT<256> m_merkleHash;      // TODO: sha256
+        
+        // Block data
+        Hash* m_hash = nullptr;
+        uint64_t m_size = 0;
         std::vector<size_t>* m_transactions = nullptr;  // TODO: replace size_t on type, which represent Transaction
-
     };
 }
 
--- src/Core/Block.cpp
+++ src/Core/Block.cpp
@@ -7,8 +7,8 @@
 
 namespace IR_CORE_NS {
 
-    Block::Block(std::string&& minedTime, uint64_t difficulty, Hash* hash, Hash* previousBlock, uint64_t nonce)
-        : m_minedTime(std::move(minedTime))
+    Block::Block(const std::string& timestamp, uint64_t difficulty, Hash* hash, Hash* previousBlock, uint64_t nonce)
+        : m_timestamp(timestamp)
         , m_difficulty(difficulty)
         , m_hash(new Hash(*hash))
         , m_previousBlock(new Hash(*previousBlock))
@@ -16,12 +16,28 @@ namespace IR_CORE_NS {
     {}
 
 
-    Block::Hash* Block::GetHash() const {
+    IR_NODISCARD Block::Hash* Block::GetHash() const {
         return m_hash;
     }
 
-    uint64_t Block::GetNonce() const {
+    IR_NODISCARD Block::Hash* Block::GetPreviousBlockHash() const {
+        return m_previousBlock;
+    }
+
+    IR_NODISCARD uint64_t Block::GetNonce() const {
         return m_nonce;
+    }
+
+    IR_NODISCARD std::string Block::GetTimestamp() const {
+        return m_timestamp;
+    }
+
+    IR_NODISCARD uint64_t Block::GetDifficulty() const {
+        return m_difficulty;
+    }
+
+    IR_NODISCARD uint64_t Block::GetSize() const {
+        return m_size;
     }
 
 
